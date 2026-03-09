7430f39ff6386a2bfd879cf09c1e948da9b5599d
ash14 <ash14@users.noreply.github.com>
2026-03-09 23:13:08 +0800

    Add test

--- packages/vscode-css-variables/src/test/completion.test.ts
+++ packages/vscode-css-variables/src/test/completion.test.ts
@@ -15,17 +15,23 @@ suite('Should do completion', () => {
   const docUri = getDocUri('test.css');
 
   test.only('Completes in css file', async () => {
-    await testCompletion(docUri, 'color: -^', {
-      items: [
-        {
-          label: '--chakra-ring-offset-width',
-          kind: vscode.CompletionItemKind.Variable,
-        },
-        {
-          label: '--chakra-ring-color',
-          kind: vscode.CompletionItemKind.Color,
-        },
-      ],
+    await testCompletion(docUri, 'color: -^', (completionList) => {
+      assertItem(completionList, {
+        label: '--chakra-ring-offset-width',
+        kind: vscode.CompletionItemKind.Variable,
+      });
+
+      assertItem(completionList, {
+        label: '--chakra-ring-color',
+        kind: vscode.CompletionItemKind.Color,
+      });
+
+      assertOrder(completionList, [
+        '--size-1',
+        '--size-2',
+        '--size-10'
+      ]);
+
     });
   });
 });
@@ -33,7 +39,7 @@ suite('Should do completion', () => {
 async function testCompletion(
   docUri: vscode.Uri,
   searchText: string,
-  expectedCompletionList: vscode.CompletionList,
+  then: (actualCompletionList: vscode.CompletionList) => void,
 ) {
   await activate(docUri);
 
@@ -47,17 +53,30 @@ async function testCompletion(
     toPosition,
   );
 
-  expectedCompletionList.items.forEach((expectedItem) => {
-    const actualItem = actualCompletionList.items.find((item) => {
-      if (typeof item.label === 'string') {
-        return item.label === expectedItem.label;
-      }
+  then(actualCompletionList);
+}
 
-      return false;
-    });
-
-    assert.ok(actualItem);
-    assert.strictEqual(actualItem.label, expectedItem.label);
-    assert.strictEqual(actualItem.kind, expectedItem.kind);
+function assertItem(actualCompletionList: vscode.CompletionList, expectedItem) {
+  const actualItem = actualCompletionList.items.find((item) => {
+    if (typeof item.label === 'string') {
+      return item.label === expectedItem.label;
+    }
   });
+
+  assert.ok(actualItem);
+  assert.strictEqual(actualItem.label, expectedItem.label);
+  assert.strictEqual(actualItem.kind, expectedItem.kind);
+}
+
+function assertOrder(actualCompletionList: vscode.CompletionList, expectedItems) {
+  const expectedItemsSet = new Set(expectedItems);
+
+  const actualOrder = actualCompletionList.items.reduce((acc, item) => {
+    if (typeof item.label === 'string' && expectedItemsSet.has(item.label)) {
+      acc.push(item.label);
+    }
+    return acc;
+  }, []);
+
+  assert.deepStrictEqual(actualOrder, expectedItems);
 }
